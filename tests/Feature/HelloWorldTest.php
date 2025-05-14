cf400d2bc6fb4d7891663230075f048c9eb635dd
Jeferson Oliveira <zuruckyt@gmail.com>
2025-05-14 01:05:42 -0300

    chore: phpcsfixer

--- tests/Feature/HelloWorldTest.php
+++ tests/Feature/HelloWorldTest.php
@@ -6,7 +6,6 @@ use Tests\TestCase;
 
 class HelloWorldTest extends TestCase
 {
-
     public function test_it_should_print_hello_world(): void
     {
         $response = (string) file_get_contents($this->baseUrl);
